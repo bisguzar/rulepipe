83f90aa35df6d55afacb674ffcedaf4db70fd248
Bugra Isguzar <biisguzar@gmail.com>
2020-02-12 20:00:11 +0300

    rewrite service with falcon

--- rulepipe/app.py
+++ rulepipe/app.py
@@ -1,34 +1,65 @@
-from flask import Flask, request
-from core.rulepipe import *
+import falcon
+import json
 
-app = Flask(__name__)
-rules = RuleManager()
+try:
+    from .core import RuleManager
+except ImportError:
+    from core import RuleManager
 
-@app.route('/')
-def root():
-  return 'Rulepipe!\n'
+api = falcon.API()
+rulemanager = RuleManager()
 
-@app.route('/add_rule/<name>', methods=['POST'])
-def add_rule(name):
-  rules.add_rule_json(name, request.get_json())
-  return 'OK\n'
 
-@app.route('/delete_rule/<name>', methods=['DELETE'])
-@app.route('/remove_rule/<name>', methods=['DELETE'])
-def delete_rule(name):
-  response = rules.delete_rule(name)
-  return {'delete_status': response}
+class HomeResource(object):
+    def on_get(self, req, resp):
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.body = "Rulepipe is alive!"
 
-@app.route('/execute_rule/<name>', methods=['POST'])
-def execute_rule(name):
-  print(request.get_json())
-  response = rules.execute_rule_json(name, request.get_json())
-  return {"response": str(response)}
 
-@app.route('/rules', methods=['GET'])
-@app.route('/get_rules', methods=['GET'])
-@app.route('/get_rule_list', methods=['GET'])
-@app.route('/get_rules_list', methods=['GET'])
-def get_rules():
-  response = rules.get_rule_list()
-  return {"rule_list": str(response)}
+class AddRuleResource(object):
+    def on_post(self, req, resp, rule_name):
+        rule = req.media
+
+        try:
+            rulemanager.add_rule_json(rule_name, rule)
+        except KeyError as err:
+            resp.status = falcon.HTTP_422
+            resp.body = json.dumps(dict(err=err.args[0]))
+            return
+        except NameError as err:
+            resp.status = falcon.HTTP_409
+            resp.body = json.dumps(
+                dict(
+                    err=f"Couldn't create new rule, rule named by {rule_name} already exists."
+                )
+            )
+            return 
+
+        resp.status = falcon.HTTP_201
+        resp.body = json.dumps(dict(msg="Rule created successfully."))
+
+
+class RuleResource(object):
+    def on_post(self, req, resp, rule_name):
+        print('adding new rule')
+        try:
+            response = rulemanager.execute_rule_json(rule_name, req.media)
+            resp.body = json.dumps(dict(msg=response))
+        except KeyError as err:
+            resp.status = falcon.HTTP_404
+            resp.body = json.dumps(dict(err=f"Rule '{rule_name}' not found."))
+
+    def on_delete(self, req, resp, rule_name):
+        response = rulemanager.delete_rule(rule_name)
+        resp.body = json.dumps(dict(msg=response))
+
+
+class RuleListResource(object):
+    def on_get(self, req, resp):
+        resp.body = json.dumps(dict(rules=rulemanager.get_rule_list()))
+
+
+api.add_route("/", HomeResource())
+api.add_route("/add_rule/{rule_name}", AddRuleResource())
+api.add_route("/rules", RuleListResource())
+api.add_route("/rules/{rule_name}", RuleResource())
